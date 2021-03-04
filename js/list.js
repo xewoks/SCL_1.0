1c211cb29e1cebd35e370597048774248d5f9b2c
xewoks <76702828+xewoks@users.noreply.github.com>
2021-03-04 10:30:49 -0500

    here to store the files

---
+++ js/list.js
@@ -0,0 +1,102 @@
+const d = {
+	"list": [
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "xEwoks",
+					"link": "link",
+					"percent": "100%",
+					"hz": "144hz"
+				},
+
+      ],
+			"name": "My Homie KrX",
+			"author": "xEwoks",
+			"more": "Update",
+			"id": "67106324",
+			"pass": "Level pass here",
+			"percentToQualify": "NaN",
+			"verificationVid": "https://www.youtube.com/watch?v=54-5wlj45wQ",
+			"key": 0
+		},
+		
+		/*=================================================================================*/
+/*=================================================================================*/
+{
+	"vids": [
+		{
+			"user": "None",
+			"link": "yt link here",
+			"percent": "remove these quotations and replace it with an integer (numbers)",
+			"hz": "refresh rate here (Make sure you add Hz at the end"
+		},
+
+],
+	"name": "KrX",
+	"author": "KaiGuy",
+	"more": "Yeah",
+	"id": "67001174",
+	"pass": "Unknown",
+	"percentToQualify": "NaN",
+	"verificationVid": "https://www.youtube.com/watch?v=fRFFbz2kUjc",
+	"key": 1
+},
+
+/*=================================================================================*/
+{
+	"vids": [
+		{
+			"user": "name here",
+			"link": "yt link here",
+			"percent": "remove these quotations and replace it with an integer (numbers)",
+			"hz": "refresh rate here (Make sure you add Hz at the end"
+		},
+
+],
+	"name": "Last Breath",
+	"author": "Maxfs (to be removed)",
+	"more": "none",
+	"id": "none",
+	"pass": "None",
+	"percentToQualify": "NaN",
+	"verificationVid": "https://www.youtube.com/watch?v=LywaFh5-OE4",
+	"key": 2
+},
+
+/*=================================================================================*/
+{
+	"vids": [
+		{
+			"user": "name here",
+			"link": "yt link here",
+			"percent": "remove these quotations and replace it with an integer (numbers)",
+			"hz": "refresh rate here (Make sure you add Hz at the end"
+		},
+
+],
+	"name": "SLXM",
+	"author": "KaiGuy",
+	"more": "Pretty easy ngl",
+	"id": "66566975",
+	"pass": "None",
+	"percentToQualify": "NaN",
+	"verificationVid": "https://www.youtube.com/watch?v=behCf3wviqs",
+	"key": 3
+},
+
+/*=================================================================================*/
+
+
+
+/*only modify version with every MAJOR update (you can always check update number at https://github.com/electroflameofficial/gdshittylist/releases*/
+
+	],
+	"version": [
+		1,
+		0,
+		0
+	]
+};
+
+const list = d.list;const version = d.version;
